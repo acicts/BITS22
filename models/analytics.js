0c7cded9897b1121ce44fa1cc92015d57ed84437
CrazyBotBoy <pasindudushan07@gmail.com>
2022-10-09 11:43:50 +0530

    Update analytics.js

--- models/analytics.js
+++ models/analytics.js
@@ -14,6 +14,11 @@ const Schema = mongoose.Schema;
     coding_tasks: Number,
     design_tasks: Number,
     explore_tasks: Number,
+    time_spend: [{ time: Number }],
+    total_page_clicks: [{ clicks: Number }],
+    total_button_clicks: [{ clicks: Number }],
+    total_link_press: [{ clicks: Number }],
+    total_mouse_movement: [{ movement: Number }]
 })),
   (Analytics = mongoose.model("Analytics", analyticSchema));
 
